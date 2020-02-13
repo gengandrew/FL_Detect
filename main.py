4b2d140ac1b77c9065918c20b15b5a6a7800612b
Andrew Geng <gengandrew88@gmail.com>
2020-02-13 13:36:20 -0600

    Added deviation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from imutils.video import VideoStream
 from imutils.video import WebcamVideoStream
 from imutils import face_utils
 from scipy.spatial import distance
+from datetime import datetime
 import datetime
 import argparse
 import imutils
@@ -13,11 +14,13 @@ import math
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("68_landmarks.dat")
+deviation = 40
 
 def getInitialCal():
     vs = VideoStream(0).start()
     time.sleep(2.0)
 
+    print("Starting initial calibration")
     while True:
         frame = vs.read()
         frame = imutils.resize(frame, width=800)
@@ -39,7 +42,6 @@ def getInitialCal():
             cv2.line(frame, (int(xlist[8]), int(ylist[8])), (int(xlist[27]), int(ylist[27])), (255, 0, 255), thickness=2)
             cv2.imshow("Frame", frame)
             if cv2.waitKey(1) & 0xFF == ord("o"):
-                distance.euclidean((xlist[2], ylist[2]), (xlist[14], ylist[14]))
                 ret = {
                     "hline": distance.euclidean((xlist[2], ylist[2]), (xlist[14], ylist[14])),
                     "vline": distance.euclidean((xlist[8], ylist[8]), (xlist[27], ylist[27]))
@@ -49,78 +51,75 @@ def getInitialCal():
                 return ret
 
 
-initCal = getInitialCal()
-vs = VideoStream(0).start()
-time.sleep(2.0)
+def dataCollection(calibration):
+    vs = VideoStream(0).start()
+    time.sleep(2.0)
+    data = []
 
-while True:
-    shouldExit = False
-    frame = vs.read()
-    frame = imutils.resize(frame, width=800)
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # rects = detector(gray, 0)
-    # print("rects1 = " + str(len(rects)))
-    rects = detector(frame, 0)
-    if len(rects) == 0:
-        print("Face detection error")
+    print("Starting data collection")
+    while True:
+        shouldExit = False
+        frame = vs.read()
+        frame = imutils.resize(frame, width=800)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # rects = detector(gray, 0)
+        # print("rects1 = " + str(len(rects)))
+        rects = detector(frame, 0)
+        currTime = datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%S.%fZ")
+        if len(rects) == 0:
+            print("Face detection error")
 
-    for rect in rects:
-        shape = predictor(frame, rect)
-        shape = face_utils.shape_to_np(shape)
-        xlist = []
-        ylist = []
-        index = 1
-        for (x, y) in shape:
-            cv2.circle(frame, (x, y), 1, (0, 0, 255), thickness=2)
-            xlist.append(float(x))
-            ylist.append(float(y))
-            index = index+1
+        for rect in rects:
+            shape = predictor(frame, rect)
+            shape = face_utils.shape_to_np(shape)
+            xylist = []
+            for (x, y) in shape:
+                cv2.circle(frame, (x, y), 1, (0, 0, 255), thickness=2)
+                xylist.append((float(x), float(y), currTime))
 
-        # Start frame streaming
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-
-        # Some normalizing calculations
-        p32 = (xlist[31], ylist[31])
-        p36 = (xlist[35], ylist[35])
-
-        xmean = np.mean(xlist)
-        ymean = np.mean(ylist)
-        xcentral = [(x-xmean) for x in xlist]
-        ycentral = [(y-ymean) for y in ylist]
-        print("At new index 49 value of ["+str(xcentral[48])+","+str(ycentral[48])+"]")
-        print("At new index 55 value of ["+str(xcentral[54])+","+str(ycentral[54])+"]")
-        
-        landmarks_vectorised = []
-        indexer = 1
-        for x, y, w, z in zip(xcentral, ycentral, xlist, ylist):
-            landmarks_vectorised.append(w)
-            landmarks_vectorised.append(z)
-            meannp = np.asarray((ymean,xmean))
-            coornp = np.asarray((z,w))
-            dist = np.linalg.norm(coornp-meannp)
-            landmarks_vectorised.append(dist)
-            landmarks_vectorised.append((math.atan2(y, x)*360)/(2*math.pi))
-            if indexer == 49 or indexer == 55:
-                print("At index " + str(indexer))
-                print(dist)
-                print((math.atan2(y, x)*360)/(2*math.pi))
+            nhline = distance.euclidean((xylist[2][0], xylist[2][1]), (xylist[14][0], xylist[14][1]))
+            nvline = distance.euclidean((xylist[8][0], xylist[8][1]), (xylist[27][0], xylist[27][1]))
+            if abs(float(calibration["hline"]) - float(nhline)) < deviation and abs(float(calibration["vline"]) - float(nvline)) < deviation:
+                data.append(xylist)
+            else:
+                print("Failed deviation check")
             
-            indexer = indexer+1
-        # for i in range(1, 68):
-        #     if(i == 49 or i == 55):
-        #         print("At new index "+str(i)+" value of ["+str(xcentral[i])+","+str(ycentral[i])+"]")
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                print(data)
+                cv2.destroyAllWindows()
+                vs.stop()
+                print("Cleanup complete")
+                return data
 
-        # Upon q key press end session
-        if key == ord("q"):
-            shouldExit = True
-            break
+
+initCal = getInitialCal()
+data = dataCollection(initCal)
+
+
+# Some normalizing calculations
+# p32 = (xlist[31], ylist[31])
+# p36 = (xlist[35], ylist[35])
+
+# xmean = np.mean(xlist)
+# ymean = np.mean(ylist)
+# xcentral = [(x-xmean) for x in xlist]
+# ycentral = [(y-ymean) for y in ylist]
+# print("At new index 49 value of ["+str(xcentral[48])+","+str(ycentral[48])+"]")
+# print("At new index 55 value of ["+str(xcentral[54])+","+str(ycentral[54])+"]")
+
+# landmarks_vectorised = []
+# indexer = 1
+# for x, y, w, z in zip(xcentral, ycentral, xlist, ylist):
+#     landmarks_vectorised.append(w)
+#     landmarks_vectorised.append(z)
+#     meannp = np.asarray((ymean,xmean))
+#     coornp = np.asarray((z,w))
+#     dist = np.linalg.norm(coornp-meannp)
+#     landmarks_vectorised.append(dist)
+#     landmarks_vectorised.append((math.atan2(y, x)*360)/(2*math.pi))
+#     if indexer == 49 or indexer == 55:
+#         print("At index " + str(indexer))
+#         print(dist)
+#         print((math.atan2(y, x)*360)/(2*math.pi))
     
-    if shouldExit:
-        break
-
-
-# Cleanup video stream and etc.
-cv2.destroyAllWindows()
-vs.stop()
-print("Cleanup complete")
+#     indexer = indexer+1
